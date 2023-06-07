d99480c2e76096d70c0217b993c7e4c12f8876b5
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-07 16:47:12 +0530

    Update add_users_and_grant_rdp.ps1

--- add_users_and_grant_rdp.ps1
+++ add_users_and_grant_rdp.ps1
@@ -12,3 +12,15 @@ Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User2"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User3"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User4"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "localadmin"
+
+
+
+# Add user to Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member "localadmin"
+
+# Add user to PreventAdmin group
+New-LocalGroup -Name "PreventAdmin" -Description "PreventAdmin group"
+
+# Add User3 and User4 to PreventAdmin group
+Add-LocalGroupMember -Group "PreventAdmin" -Member "User3"
+Add-LocalGroupMember -Group "PreventAdmin" -Member "User4"
