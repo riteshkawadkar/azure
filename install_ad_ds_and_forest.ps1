0be3b2ea9db272e638189deae378488e4c604833
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-07 19:17:33 +0530

    Update install_ad_ds_and_forest.ps1

--- install_ad_ds_and_forest.ps1
+++ install_ad_ds_and_forest.ps1
@@ -2,6 +2,8 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment 
 Install-ADDSForest -DomainName "test.local" -DomainMode "WinThreshold" -ForestMode "WinThreshold" -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "NewMinal@123" -Force) -Force
 
+# Restart the VM
+Restart-Computer
 
 $groupNames = "LabUsers", "ITUsers", "HODUsers"
 
