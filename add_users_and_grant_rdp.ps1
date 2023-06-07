6019aa56614770859c34909d77dced118efba856
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-07 16:34:28 +0530

    Update add_users_and_grant_rdp.ps1

--- add_users_and_grant_rdp.ps1
+++ add_users_and_grant_rdp.ps1
@@ -5,15 +5,6 @@ New-LocalUser -Name "User3" -Password (ConvertTo-SecureString "123456" -AsPlainT
 New-LocalUser -Name "User4" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "User Four" -Description "User Four account"
 New-LocalUser -Name "localadmin" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "Local Admin" -Description "Local Admin account"
 
-# Add user to Administrators group
-Add-LocalGroupMember -Group "Administrators" -Member "localadmin"
-
-# Add user to PreventAdmin group
-New-LocalGroup -Name "PreventAdmin" -Description "PreventAdmin group"
-
-# Add User3 and User4 to PreventAdmin group
-Add-LocalGroupMember -Group "PreventAdmin" -Member "User3"
-Add-LocalGroupMember -Group "PreventAdmin" -Member "User4"
 
 # Grant RDP access
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User1"
