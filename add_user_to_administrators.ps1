af0a7a8ee66547acd62f70c4561cdc0024bb35c4
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-07 16:34:42 +0530

    Update add_user_to_administrators.ps1

--- add_user_to_administrators.ps1
+++ add_user_to_administrators.ps1
@@ -1,2 +1,10 @@
 # Add user to Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member "localadmin"
+
+
+# Add user to PreventAdmin group
+New-LocalGroup -Name "PreventAdmin" -Description "PreventAdmin group"
+
+# Add User3 and User4 to PreventAdmin group
+Add-LocalGroupMember -Group "PreventAdmin" -Member "User3"
+Add-LocalGroupMember -Group "PreventAdmin" -Member "User4"
