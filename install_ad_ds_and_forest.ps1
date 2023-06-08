525fa2b58c70bd16505b93e7681e7d0975392163
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-08 12:22:15 +0530

    Update install_ad_ds_and_forest.ps1

--- install_ad_ds_and_forest.ps1
+++ install_ad_ds_and_forest.ps1
@@ -3,5 +3,5 @@ Import-Module ADDSDeployment
 Install-ADDSForest -DomainName "test.local" -DomainMode "WinThreshold" -ForestMode "WinThreshold" -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "NewMinal@123" -Force) -Force -NoRebootOnCompletion:$true
 
 # Restart the computer and wait for it to come back online
-Restart-Computer -Wait -For PowerShell
+Restart-Computer
 
