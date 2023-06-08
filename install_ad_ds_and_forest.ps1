66e0c2437f72dba0708c51663b99075757c3047e
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-08 11:01:20 +0530

    Update install_ad_ds_and_forest.ps1

--- install_ad_ds_and_forest.ps1
+++ install_ad_ds_and_forest.ps1
@@ -5,28 +5,3 @@ Install-ADDSForest -DomainName "test.local" -DomainMode "WinThreshold" -ForestMo
 # Restart the computer and wait for it to come back online
 Restart-Computer -Wait -For PowerShell
 
-$groupNames = "LabUsers", "ITUsers", "HODUsers"
-
-foreach ($groupName in $groupNames) {
-    New-ADGroup -Name $groupName -GroupScope Global -GroupCategory Security
-}
-
-$password = ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force
-
-foreach ($groupName in $groupNames) {
-    for ($i = 1; $i -le 2; $i++) {
-        $username = "$($groupName)_User$i"
-        New-ADUser -Name $username -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
-        Add-ADGroupMember -Identity $groupName -Members $username
-    }
-}
-
-$rdpGroupName = "Remote Desktop Users"
-
-foreach ($groupName in $groupNames) {
-    $groupMembers = Get-ADGroupMember -Identity $groupName
-
-    foreach ($user in $groupMembers) {
-        Add-LocalGroupMember -Group $rdpGroupName -Member $user.SamAccountName
-    }
-}
