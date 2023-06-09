c5611253ea92fc062f820059bc74c1c631854151
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2023-06-09 11:46:24 +0530

    Update add_users.ps1

--- add_users.ps1
+++ add_users.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy RemoteSigned
+
 Install-WindowsFeature RSAT-AD-PowerShell
 Import-Module ActiveDirectory
 
@@ -22,19 +24,17 @@ foreach ($computerName in $computerNames) {
 
 Write-Host "OU and computers created successfully."
 
-
-
 $groupNames = "Lab", "IT", "HOD"
 
 foreach ($groupName in $groupNames) {
     New-ADGroup -Name $groupName -GroupScope Global -GroupCategory Security
 }
 
-$password = ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force
+$password = ConvertTo-SecureString -String "NewMinal@123" -AsPlainText -Force
 
 foreach ($groupName in $groupNames) {
     for ($i = 1; $i -le 2; $i++) {
-        $username = "$($groupName)User$i"
+        $username = "$groupName" + "User$i"
         New-ADUser -Name $username -AccountPassword $password -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
         Add-ADGroupMember -Identity $groupName -Members $username
     }
