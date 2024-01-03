a582a375661cfc0cafa60dcb6b20c4a21e94f773
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-03 19:20:35 +0530

    Update join-domain.ps1

--- join-domain.ps1
+++ join-domain.ps1
@@ -1,7 +1,7 @@
 $domain = "mylab.local"
 $username = "adminuser"
-$password = "NewMinal@123" | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $domainUser, $securePassword
+$securePassword = "NewMinal@123" | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
 Add-Computer -DomainName $domain -Credential $credential
 
