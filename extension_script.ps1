e35037623b6b06eb54e780fa8e217fcfe6617fa3
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-03 21:39:07 +0530

    Delete extension_script.ps1

--- extension_script.ps1
+++
@@ -1,7 +0,0 @@
-# extension_script.ps1
-$domainName="mylab.local"
-$adminUsername="adminuser"
-$adminPassword="NewMinal@123" 
-$securePassword = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$domain\$adminPassword", $password)
-Add-Computer -DomainName $domainName -Credential $credential
