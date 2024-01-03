99c7e0dc2d1bc18640b5768b58974fedf3d7e9b7
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-03 19:58:04 +0530

    Update extension_script.ps1

--- extension_script.ps1
+++ extension_script.ps1
@@ -1,6 +1,7 @@
 # extension_script.ps1
-adminUsername="adminuser"
-adminPassword="NewMinal@123" 
+$domainName="mylab.local"
+$adminUsername="adminuser"
+$adminPassword="NewMinal@123" 
 $securePassword = ConvertTo-SecureString -String "$adminPassword" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$adminUsername", "$securePassword")
-Add-Computer -DomainName 'mylab.local' -Credential $credential
+Add-Computer -DomainName $domainName -Credential $credential
