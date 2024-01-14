9292f0355f2aed6137b05cddd0f636da1d7607b4
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-14 13:51:45 +0530

    Create create-ou-group-user.ps1

---
+++ ActiveDirectry_Lab_Scripts/create-ou-group-user.ps1
@@ -0,0 +1,98 @@
+# This script performs three tasks:
+# 1. Creating Organizational Units (OUs) from a CSV file
+# 2. Creating Groups from a CSV file
+# 3. Creating Users from a CSV file
+
+# Task 1: Creating Organizational Units (OUs)
+# Import Active Directory module for running AD cmdlets
+Import-Module ActiveDirectory
+  
+# Store the data from ous.csv in the $ADOU variable. CSV template needs the following headers -> name, path
+$ADOU = Import-Csv ous.csv
+
+# Loop through each row containing OU details in the CSV file
+foreach ($ou in $ADOU)
+{
+    # Read data from each field in each row and assign the data to a variable
+    $name = $ou.name
+    $path = $ou.path
+
+    # Create an Organizational Unit in the specified path
+    New-ADOrganizationalUnit -Name $name -Path $path
+} 
+
+# Add a wait condition between tasks
+Start-Sleep -Seconds 10
+
+# Task 2: Creating Groups
+# Import Active Directory module for running AD cmdlets
+Import-Module ActiveDirectory
+
+# Import CSV containing group details
+$groups = Import-Csv groups.csv
+
+# Loop through the CSV to create groups
+foreach ($group in $groups) {
+    $groupProps = @{
+        Name          = $group.name
+        Path          = $group.path
+        GroupScope    = $group.scope
+        GroupCategory = $group.category
+        Description   = $group.description
+    }
+
+    New-ADGroup @groupProps
+}
+
+# Add a wait condition between tasks
+Start-Sleep -Seconds 10
+
+# Task 3: Creating Users
+# Import Active Directory module for running AD cmdlets
+Import-Module ActiveDirectory
+
+# Store the data from users.csv in the $Users variable
+$Users = Import-Csv users.csv
+
+# Loop through each row containing user details in the CSV file 
+foreach ($User in $Users) {
+    $Username = $User.SamAccountName
+
+    # Check if the user already exists in AD
+    if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
+        Write-Warning "A user account with username $Username already exists in Active Directory."
+    }
+    else {
+        # User does not exist, proceed to create the new user account
+        $userProps = @{
+            SamAccountName             = $User.SamAccountName                   
+            Path                       = $User.Path      
+            GivenName                  = $User.GivenName 
+            Surname                    = $User.Surname
+            Initials                   = $User.Initials
+            Name                       = $User.Name
+            DisplayName                = $User.DisplayName
+            UserPrincipalName          = $User.UserPrincipalName 
+            Department                 = $User.Department
+            Description                = $User.Description
+            Office                     = $User.Office
+            OfficePhone                = $User.OfficePhone
+            StreetAddress              = $User.StreetAddress
+            POBox                      = $User.POBox
+            City                       = $User.City
+            State                      = $User.State
+            PostalCode                 = $User.PostalCode
+            Title                      = $User.Title
+            Company                    = $User.Company
+            Country                    = $User.Country
+            EmailAddress               = $User.Email
+            AccountPassword            = (ConvertTo-SecureString $User.Password -AsPlainText -Force) 
+            Enabled                    = $true
+            ChangePasswordAtLogon      = $true
+        }
+
+        New-ADUser @userProps
+    }
+}
+
+# End of script
