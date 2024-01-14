47745ad89bf56d5b3b29e92c12cbb9ca856aa872
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-14 16:35:19 +0530

    Update add-iis.ps1

--- add-iis.ps1
+++ add-iis.ps1
@@ -1,5 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 # Install IIS
-Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools -Include Web-Windows-Auth, Web-Url-Auth, Web-Net-Ext, Web-ASP, Web-ISAPI-Ext, Web-Includes
 
 # Start the World Wide Web Publishing Service
 Start-Service W3SVC
