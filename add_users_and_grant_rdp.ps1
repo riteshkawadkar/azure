e1322a065c6d12ce578ab59b86e839ebc5dd296e
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-26 20:45:31 +0530

    Update add_users_and_grant_rdp.ps1

--- add_users_and_grant_rdp.ps1
+++ add_users_and_grant_rdp.ps1
@@ -1,9 +1,9 @@
 # Add users
-New-LocalUser -Name "User1" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "User One" -Description "User One account"
-New-LocalUser -Name "User2" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "User Two" -Description "User Two account"
-New-LocalUser -Name "User3" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "User Three" -Description "User Three account"
-New-LocalUser -Name "User4" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "User Four" -Description "User Four account"
-New-LocalUser -Name "localadmin" -Password (ConvertTo-SecureString "123456" -AsPlainText -Force) -FullName "Local Admin" -Description "Local Admin account"
+New-LocalUser -Name "User1" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User One" -Description "User One account"
+New-LocalUser -Name "User2" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User Two" -Description "User Two account"
+New-LocalUser -Name "User3" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User Three" -Description "User Three account"
+New-LocalUser -Name "User4" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User Four" -Description "User Four account"
+New-LocalUser -Name "localadmin" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "Local Admin" -Description "Local Admin account"
 
 
 # Grant RDP access
@@ -19,8 +19,8 @@ Add-LocalGroupMember -Group "Remote Desktop Users" -Member "localadmin"
 Add-LocalGroupMember -Group "Administrators" -Member "localadmin"
 
 # Add user to PreventAdmin group
-New-LocalGroup -Name "PreventAdmin" -Description "PreventAdmin group"
+New-LocalGroup -Name "NonGxPAdmin" -Description "Non GxP Admin group"
 
 # Add User3 and User4 to PreventAdmin group
-Add-LocalGroupMember -Group "PreventAdmin" -Member "User3"
-Add-LocalGroupMember -Group "PreventAdmin" -Member "User4"
+Add-LocalGroupMember -Group "NonGxPAdmin" -Member "User3"
+Add-LocalGroupMember -Group "NonGxPAdmin" -Member "User4"
