da538b2a677880a754512b36bf80481c6f37cb3f
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-26 20:39:22 +0530

    Update and rename add-iis.ps1 to add-iis-and-install-apps.ps1

--- add-iis-and-install-apps.ps1
+++ add-iis-and-install-apps.ps1
@@ -1,24 +1,3 @@
-# Check if .NET Framework 4.8 is installed
-$dotNetFrameworkVersion = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' -Name Release
-if ($dotNetFrameworkVersion.Release -ge 528040) {
-    Write-Host ".NET Framework 4.8 is already installed."
-} else {
-    # Download and install .NET Framework 4.8
-    $dotNetFrameworkInstallerUrl = 'https://download.visualstudio.microsoft.com/download/pr/3a3ff58d-2043-4dc9-a47f-44e703cd9a36/06b0170df2fca8935cd0ef002d25a5f1/ndp48-x86-x64-allos-enu.exe'
-    $dotNetFrameworkInstallerPath = "$env:TEMP\dotNetFrameworkInstaller.exe"
-
-    Invoke-WebRequest -Uri $dotNetFrameworkInstallerUrl -OutFile $dotNetFrameworkInstallerPath -UseBasicParsing
-    Start-Process -FilePath $dotNetFrameworkInstallerPath -ArgumentList '/quiet', '/norestart' -Wait
-    Remove-Item -Path $dotNetFrameworkInstallerPath -Force
-}
-
-# Install Chocolatey
-if ((Get-Command -Name 'choco' -ErrorAction SilentlyContinue) -eq $null) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-} else {
-    Write-Host "Chocolatey is already installed."
-}
-
 # choco installation
 # https://chocolatey.org/install
 
