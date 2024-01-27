15465877cc57cbb60d19534372d8dd5f08ff6016
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-27 18:20:17 +0530

    Update join-domain.ps1

--- join-domain.ps1
+++ join-domain.ps1
@@ -8,5 +8,13 @@ $domainUsername = "mylab.local\adminuser"  # Replace DOMAIN\username with actual
 $domainPassword = "NewMinal@123" | ConvertTo-SecureString -AsPlainText -Force  # Replace password with actual domain admin password
 $credential = New-Object System.Management.Automation.PSCredential($domainUsername, $domainPassword)
 
+
+
+# Add "Domain Users" to the Remote Desktop Users group
+$domainUsersGroup = "mylab\Domain Users"
+$rdpUsersGroup = "BUILTIN\Remote Desktop Users"
+Add-LocalGroupMember -Group $rdpUsersGroup -Member $domainUsersGroup
+
+
 # Join the domain
 Add-Computer -DomainName $domain -Credential $credential -Force -Restart
