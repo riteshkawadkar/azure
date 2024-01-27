74a21618692007f2231c7afeb87e7f9a84d361df
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-01-27 11:05:23 +0530

    Update add_users_and_grant_rdp.ps1

--- add_users_and_grant_rdp.ps1
+++ add_users_and_grant_rdp.ps1
@@ -3,7 +3,7 @@ New-LocalUser -Name "User1" -Password (ConvertTo-SecureString "NewMinal@123" -As
 New-LocalUser -Name "User2" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User Two" -Description "User Two account"
 New-LocalUser -Name "User3" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User Three" -Description "User Three account"
 New-LocalUser -Name "User4" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "User Four" -Description "User Four account"
-New-LocalUser -Name "localadmin" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "Local Admin" -Description "Local Admin account"
+# New-LocalUser -Name "localadmin" -Password (ConvertTo-SecureString "NewMinal@123" -AsPlainText -Force) -FullName "Local Admin" -Description "Local Admin account"
 
 
 # Grant RDP access
@@ -11,12 +11,12 @@ Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User1"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User2"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User3"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "User4"
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "localadmin"
+# Add-LocalGroupMember -Group "Remote Desktop Users" -Member "localadmin"
 
 
 
 # Add user to Administrators group
-Add-LocalGroupMember -Group "Administrators" -Member "localadmin"
+# Add-LocalGroupMember -Group "Administrators" -Member "localadmin"
 
 # Add user to PreventAdmin group
 New-LocalGroup -Name "NonGxPAdmin" -Description "Non GxP Admin group"
